3a6cd450de26d28aa6d7d2d9d213e1f13edb74bc
thonglt12 <thonglt1234@gmail.com>
2023-10-15 23:24:00 +0700

    Initial commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=thonglt
+export POSTGRES_PASSWORD=Actvn123
+export POSTGRES_HOST=postgres.ckp5tpc1q5jp.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::mybucket-487854660273
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
+export AWS_ACCESS_KEY_ID=ASIAXDFTHLKY6424U7OU
+export AWS_SECRET_ACCESS_KEY=1FZj7axZf8UwNXg1MmBWbBkkJg1Vjv1RIPfKH0hY
